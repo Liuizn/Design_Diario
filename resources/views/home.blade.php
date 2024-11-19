02df19a82e06bfc95d2119e9a3ee03e8f5264b48
mathieuz <matheusandrewww@gmail.com>
2024-11-19 00:06:38 -0300

    feat: autores populares sessão

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex flex-col">
-        <div class="flex flex-col flex-wrap items-center align-center self-center w-fit">
+        <div class="flex flex-col flex-wrap items-center align-center self-center w-fit mb-20">
             <div class="flex flex-row flex-wrap justify-center w-auto">
                 <x-post-card postImageCover="https://149361125.v2.pressablecdn.com/wp-content/uploads/2018/09/Sala-1.jpg" postTitle="Design de Interiores: Tendências de 2025" authorName="Lucas Ramil" authorProfilePic="https://img.freepik.com/fotos-gratis/retrato-de-homem-branco-isolado_53876-40306.jpg" postUrl="" fullWidth="false"/>
                 <x-post-card postImageCover="https://149361125.v2.pressablecdn.com/wp-content/uploads/2018/09/Sala-1.jpg" postTitle="Design de Interiores: Tendências de 2025" authorName="Lucas Ramil" authorProfilePic="https://img.freepik.com/fotos-gratis/retrato-de-homem-branco-isolado_53876-40306.jpg" postUrl="" fullWidth="false"/>
@@ -13,6 +13,11 @@
             </div>
             <div class="flex flex-row flex-wrap justify-center w-auto">
                 <x-post-card postImageCover="https://149361125.v2.pressablecdn.com/wp-content/uploads/2018/09/Sala-1.jpg" postTitle="Design de Interiores: Tendências de 2025" authorName="Lucas Ramil" authorProfilePic="https://img.freepik.com/fotos-gratis/retrato-de-homem-branco-isolado_53876-40306.jpg" postUrl="" fullWidth="true"/>
+            </div>
+        </div>
+        <div class="flex flex-col flex-wrap pt-4">
+            <div class="flex flex-row align-center">
+                <span class="text-3xl font-bold italic bg-lime-300 text-end pr-4 p-1 rounded-tr-lg rounded-br-lg shadow-md" style="width: 400px;">Autores Populares</span>
             </div>
         </div>
     </div>
