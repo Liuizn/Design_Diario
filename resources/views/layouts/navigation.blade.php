ac44bcd56af197b9d727adda5040fd3c1bec12ea
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2024-11-21 22:36:47 -0300

    homologando tela de artigos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,33 +2,33 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-black" />
-                    </a>
-                </div>
+            <!-- Logo -->
+            <div class="shrink-0 flex items-center">
+                <a href="{{ route('dashboard') }}">
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-black" />
+                </a>
+            </div>
 
-                <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <!-- Navigation Links -->
+            {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-black dark:text-black">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div> --}}
-                <div class="shirnk-0 flex items-center">
-                    <a href="{{route('profile.edit')}}" class="font-semibold">
-                        {{ __('Meu perfil') }}
-                    </a>
+            <div class="shirnk-0 flex items-center">
+                <a href="{{ route('profile.edit') }}" class="font-semibold">
+                    {{ __('Meu perfil') }}
+                </a>
 
-                    <div>
+                <div>
 
-                    </div>
                 </div>
+            </div>
         </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
@@ -52,7 +52,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
